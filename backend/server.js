1976c3b779fb016d525a3fa82c3a6f77a3970324
anco-sam-franco-b <ancosamfranco250@gmail.com>
2026-05-11 15:14:49 +0200

    chore: initialize backend project dependencies and package lock file

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app=express()
+const port=4000
+
+app.get('/', (req, res)=>{
+    res.send("hello server!")
+})
+app.get('/about', (req, res)=>{
+    res.send("This is our about route")
+})
+
+//starting dev server
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
