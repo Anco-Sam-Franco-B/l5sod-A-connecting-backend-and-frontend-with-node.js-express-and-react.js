6c290e9af55959df43090e32626ff2e756f7a83f
anco-sam-franco-b <ancosamfranco250@gmail.com>
2026-05-12 09:34:53 +0200

    implementing database into server and perform CRUD operation

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,32 @@
 const express = require("express")
+const cors=require('cors')
 const con=require('./Database')
 const app=express()
 const port=4000
 
-app.get('/', (req, res)=>{
-    res.send("hello server!")
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+//API ROUTES FOR CRUD OPERATIONS HTTP METHODS [GET, POST, PUT/PATCH, DELETE]
+app.get('/books', (req, res)=>{
+    con.query('SELECT * FROM books', (err, data)=>{
+        if(err){
+            return res.status(500).json({
+                message: 'Internal Server Error',
+                errorMessage: err.message
+            })
+        }
+
+        return res.status(200).json({
+            message: 'Books Fetched!',
+            result: data
+        })
+    })
 })
-app.get('/about', (req, res)=>{
-    res.send("This is our about route")
-})
+
+
 
 //starting dev server
 app.listen(port, ()=>{
